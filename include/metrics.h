7040ee0bd4f059f6f510bdab2c6a41f98c54b547
dariocastillo11 <dario.4979102@gmail.com>
2024-10-17 03:21:53 -0300

    v3

--- include/metrics.h
+++ include/metrics.h
@@ -29,3 +29,20 @@ double get_memory_usage();
  * @return Uso de CPU como porcentaje (0.0 a 100.0), o -1.0 en caso de error.
  */
 double get_cpu_usage();
+
+double get_disk_usage();
+double get_active_processes();
+
+/**
+ * @brief Obtiene el tráfico de red desde /proc/net/dev.
+ *
+ * @return Tráfico de red en bytes, o -1.0 en caso de error.
+ */
+double get_network_traffic();
+
+/**
+ * @brief Obtiene el número de cambios de contexto desde /proc/stat.
+ *
+ * @return Número de cambios de contexto, o -1.0 en caso de error.
+ */
+double get_context_switches();
--- src/main.c
+++ src/main.c
@@ -2,30 +2,46 @@
  * @file main.c
  * @brief Entry point of the system
  */
-
-#include "expose_metrics.h"
+#include "/home/dario/Escritorio/tp jueves/so-i-24-dariocastillo11/include/expose_metrics.h"
+// #include "expose_metrics.h"
 #include <stdbool.h>
 
 #define SLEEP_TIME 1
 
-int main(int argc, char* argv[])
+int main()
 {
+
+    // init_metrics();
+
+    if (init_metrics() != EXIT_SUCCESS)
+    {
+        fprintf(stderr, "Error al inicializar las metricas\n");
+        return EXIT_FAILURE; // Salir si la inicialización falla
+    }
+    fprintf(stderr, "metricas inicializadas\n");
 
     // Creamos un hilo para exponer las métricas vía HTTP
     pthread_t tid;
     if (pthread_create(&tid, NULL, expose_metrics, NULL) != 0)
     {
-        fprintf(stderr, "Error al crear el hilo del servidor HTTP\n");
+        fprintf(stderr, "Error al crear el hilo del servidor para exponer metricas HTTP\n");
         return EXIT_FAILURE;
     }
-
+    fprintf(stderr, "hilo creado con exito\n");
     // Bucle principal para actualizar las métricas cada segundo
     while (true)
     {
+        // fprintf(stderr, "entro al while\n");
         update_cpu_gauge();
         update_memory_gauge();
+        update_disk_gauge();             // Nueva métrica de disco
+        update_active_processes_gauge(); // Nueva métrica de procesos
+        update_network_traffic_gauge();
+        update_context_switches_gauge();
+
+        
         sleep(SLEEP_TIME);
     }
-
-    return EXIT_SUCCESS;
+    fprintf(stderr, "  FIN\n");
+    return 0;
 }
